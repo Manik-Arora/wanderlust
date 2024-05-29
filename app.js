491688d7893f01bd1224c27e651050c9fcce843e
Manik-Arora <manikarora2605@gmail.com>
2024-05-29 16:02:35 +0530

    Applied basic styling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const { MONGO_URL } = require("./constants");
+const ejsMate = require("ejs-mate");
 
 main()
   .then(() => {
@@ -22,6 +23,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Hi, there!");
